b34c41ec43fb2285d6ff383e61416ed7916fb449
Taro L. Saito <leo@xerial.org>
2023-06-15 15:01:59 -0700

    Closes #5: Add a sample interval data and reader (#6)

---
+++ src/test/scala/wvlet/querybase/JobIntervalTest.scala
@@ -0,0 +1,12 @@
+package wvlet.querybase
+
+import wvlet.airspec.AirSpec
+
+class JobIntervalTest extends AirSpec:
+  test("read json file") {
+    val lst = JobInterval.loadFromJson("data/sample_jobs.json")
+    lst shouldBe List(
+      JobInterval("job 1", 100, 200),
+      JobInterval("job 2", 110, 150)
+    )
+  }
