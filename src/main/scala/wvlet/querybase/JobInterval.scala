e71d077d34b4ed7538951d9ea50d0c9bca254168
Taro L. Saito <leo@xerial.org>
2023-06-15 15:20:27 -0700

    Add support for reading Parquet data

--- src/main/scala/wvlet/querybase/JobInterval.scala
+++ src/main/scala/wvlet/querybase/JobInterval.scala
@@ -4,6 +4,8 @@ import wvlet.airframe.codec.MessageCodec
 import wvlet.log.LogSupport
 
 import java.io.File
+import java.sql.DriverManager
+import scala.util.Using
 
 case class JobInterval(name: String, override val start: Long, override val end: Long) extends Interval(start, end) {
   override def toString: String = s"${name}:[${start}, ${end})"
@@ -16,5 +18,24 @@ object JobInterval extends LogSupport {
     debug(jsonData)
     val jobList = MessageCodec.of[Seq[JobInterval]].fromJson(jsonData)
     jobList
+  }
+
+  def loadFromParquet(parquetFilePath: String): Seq[JobInterval] = {
+    Class.forName("org.duckdb.DuckDBDriver")
+    val lst = Seq.newBuilder[JobInterval]
+    Using(DriverManager.getConnection("jdbc:duckdb:")) { conn =>
+      Using(conn.createStatement()) { stmt =>
+        Using(stmt.executeQuery(s"select * from '${parquetFilePath}'")) { rs =>
+          while (rs.next()) {
+            lst += JobInterval(
+              name = rs.getString(1),
+              start = rs.getLong(2),
+              end = rs.getLong(3)
+            )
+          }
+        }
+      }
+    }
+    lst.result()
   }
 }
