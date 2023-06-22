296364fda234ee96d1fab12ee17f951588e20620
Taro L. Saito <leo@xerial.org>
2023-06-22 16:24:29 -0700

    Add navbar

--- querybase-ui/src/main/scala/wvlet/querybase/ui/QuerybaseUIMain.scala
+++ querybase-ui/src/main/scala/wvlet/querybase/ui/QuerybaseUIMain.scala
@@ -25,20 +25,42 @@ object QuerybaseUIMain {
 
 import wvlet.airframe.rx.html.*
 import wvlet.airframe.rx.html.all.*
+import wvlet.airframe.rx.html.{svgTags, svgAttrs}
+
+class NavBar extends RxElement {
+  override def render: RxElement =
+    nav(
+      cls -> "navbar bg-dark border-bottom bottom-dark",
+      a(
+        cls  -> "navbar-brand text-light mx-3",
+        href -> "#",
+        "Querybase"
+      )
+    )
+}
 
 class QuerybaseUI extends RxElement {
   private val rpcClient = ServiceRPC.newRPCAsyncClient(Http.client.newJSClient)
 
-  def render = div(
-    cls -> "container",
+  override def render = div(
     div(
-      cls -> "text-primary",
-      "Hello Querybase!",
-      table(
+      NavBar(),
+      svgTags.svg(
+        svgTags.rect(
+          svgAttrs.x      -> 0,
+          svgAttrs.y      -> 0,
+          svgAttrs.width  -> 1800,
+          svgAttrs.height -> 200,
+          svgAttrs.fill   -> "#EEEEEE"
+        ),
         rpcClient.JobIntervalApi.getIntervals(TargetTimeRange(1, 1000)).map { result =>
           result.intervals.map { x =>
-            tr(
-              td(x.toString)
+            svgTags.rect(
+              svgAttrs.x      -> x.created_time,
+              svgAttrs.y      -> 0,
+              svgAttrs.width  -> (x.finished_time - x.created_time).toInt,
+              svgAttrs.height -> 20,
+              svgAttrs.fill   -> "pink"
             )
           }
         }
